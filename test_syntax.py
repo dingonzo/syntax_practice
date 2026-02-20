4a9f757be23d798751078f4b331bc6ea260de5c6
Edgar Gonzalez <245eddy.g@gmail.com>
2026-02-19 23:36:01 -0500

    Add tests for logical not and comparison

--- test_syntax.py
+++ test_syntax.py
@@ -1,1 +1,11 @@
+def test_logical_not():
+    # Practicing alternatives to '!'
+    is_valid = True
+    assert not is_valid == False  # Using 'not' instead of '!'
+    assert is_valid is not False  # Using 'is not'
+
+def test_comparison():
+    x = 10
+    y = 20
+    assert x != y  # Standard 'not equal' syntax
 
